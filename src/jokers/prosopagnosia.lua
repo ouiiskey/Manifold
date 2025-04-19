d02dcb7719cad12b2e51a3b697aca1033f33916b
ouiiskey <118245217+ouiiskey@users.noreply.github.com>
2025-04-18 23:04:06 -0400

    Squashed some Prosopagnosia bugs and moved PROSO population to always be after registration of all modded ranks

--- src/jokers/prosopagnosia.lua
+++ src/jokers/prosopagnosia.lua
@@ -5,39 +5,44 @@ PROSO = {
     next = {}
 }
 
-for k, v in pairs(SMODS.Ranks) do
-    if v.face then
-        table.insert(PROSO.faces, k)
-        for _, key in ipairs(v.next) do
-            if not SMODS.Ranks[key].face then
-                table.insert(PROSO.after_face, key)
-            end
-        end
-    end
-end
-
-for k, v in pairs(SMODS.Ranks) do
-    if v.face then
-        PROSO.next[k] = PROSO.after_face
-    else
-        local before_face = false
-        for _, key in ipairs(v.next) do
-            if SMODS.Ranks[key].face then
-                before_face = true
-                break
-            end
-        end
-        if before_face then
-            PROSO.next[k] = SMODS.shallow_copy(PROSO.faces)
+-- For compatibility purposes, we hook Game.main_menu
+local old = Game.main_menu
+Game.main_menu = function(change_context)
+    for k, v in pairs(SMODS.Ranks) do
+        if v.face then
+            table.insert(PROSO.faces, k)
             for _, key in ipairs(v.next) do
                 if not SMODS.Ranks[key].face then
-                    table.insert(PROSO.next[k], key)
+                    table.insert(PROSO.after_face, key)
                 end
             end
-        else
-            PROSO.next[k] = v.next
         end
     end
+
+    for k, v in pairs(SMODS.Ranks) do
+        if v.face then
+            PROSO.next[k] = PROSO.after_face
+        else
+            local before_face = false
+            for _, key in ipairs(v.next) do
+                if SMODS.Ranks[key].face then
+                    before_face = true
+                    break
+                end
+            end
+            if before_face then
+                PROSO.next[k] = SMODS.shallow_copy(PROSO.faces)
+                for _, key in ipairs(v.next) do
+                    if not SMODS.Ranks[key].face then
+                        table.insert(PROSO.next[k], key)
+                    end
+                end
+            else
+                PROSO.next[k] = v.next
+            end
+        end
+    end
+    old(change_context)
 end
 
 SMODS.Joker {
