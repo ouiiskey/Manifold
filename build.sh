e5520db92842ad5831c1349e7cd97971b702530e
ouiiskey <118245217+ouiiskey@users.noreply.github.com>
2026-03-20 13:00:05 -0700

    Update to smods 1.0.0-beta-1503a - No gameplay changes - Fixes #1

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 line=$(grep version manifold.json)
 version=${line:14:-2}
 
-python assets.py
+python3 assets.py
 zip -FSqr builds/Manifold-"$version".zip manifold.json README.md src assets lovely localization
 
 echo Built Manifold v"$version"
