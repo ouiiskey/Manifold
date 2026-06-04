bc5231b7dc9738af7efece8485a04d57cfe2b6fd
ouiiskey <118245217+ouiiskey@users.noreply.github.com>
2026-06-03 21:15:59 -0700

    remembered f strings

--- gradient.py
+++ gradient.py
@@ -2,5 +2,5 @@ dark = 0.32549
 light = 0.75686
 
 for i in range(16):
-    hue = format(dark + i * (light - dark) / 15, ".4f")
-    print("vec3(" + hue + ")", end = ",\n" if i < 15 else "\n")
+    hue = dark + i * (light - dark) / 15
+    print(f"vec3({hue:.4f})", end = ",\n" if i < 15 else "\n")
