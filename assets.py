021b1059d8223d3b583a21f60fdb4cb48a78bc68
ouiiskey <118245217+ouiiskey@users.noreply.github.com>
2026-06-03 21:18:36 -0700

    update assets.py to use f string too

--- assets.py
+++ assets.py
@@ -5,6 +5,6 @@ import cv2
 filenames = os.listdir("assets/1x")
 
 for name in filenames:
-    image = cv2.imread("assets/1x/" + name, cv2.IMREAD_UNCHANGED)
+    image = cv2.imread(f"assets/1x/{name}", cv2.IMREAD_UNCHANGED)
     image = cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_NEAREST)
-    cv2.imwrite("assets/2x/" + name, image)
+    cv2.imwrite(f"assets/2x/{name}", image)
