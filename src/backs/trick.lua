f509470a8226846f5fa61f812abaca9004f546e1
ouiiskey <118245217+ouiiskey@users.noreply.github.com>
2026-08-29 18:44:41 -0700

    Update function based silencing for SMODS.destroy_cards

--- src/backs/trick.lua
+++ src/backs/trick.lua
@@ -43,7 +43,7 @@ SMODS.Back {
             end,
             -- Hanged Man
             function(card)
-                SMODS.destroy_cards(card, {immediate = true, should_silence = function() return true end})
+                SMODS.destroy_cards(card, {immediate = true, silent = true})
             end,
             -- Devil
             enhance("m_gold"),
