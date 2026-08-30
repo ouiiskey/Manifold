1abc6b9f9092c0efa756aeec53ea34242ee46aae
ouiiskey <118245217+ouiiskey@users.noreply.github.com>
2026-08-29 19:16:20 -0700

    Enable costs of less than 1 in shop

--- src/jokers/monkeys_paw.lua
+++ src/jokers/monkeys_paw.lua
@@ -1,4 +1,4 @@
--- Monkey's Paw, see also monkeys_paw.toml
+-- Monkey's Paw, see also force_queue.toml
 local threshold = 200
 local sources = {"jud", "manifold_lib",
 } -- Hook: Monkey's Paw banned pools
