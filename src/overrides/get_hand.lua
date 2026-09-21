02214f8811929cfdfafab8938703fd6af7cc1206
ouiiskey <118245217+ouiiskey@users.noreply.github.com>
2026-09-20 20:54:23 -0700

    Simplify

--- src/overrides/get_hand.lua
+++ src/overrides/get_hand.lua
@@ -279,7 +279,7 @@ SMODS.PokerHandPart {
             local valid = false
             for k, v in pairs(rankmap) do
                 quick_merge(any, v)
-                if #v == 2 or #v >= 3 and ranked > #v + 1 then
+                if #v >= 2 and ranked > #v + 1 then
                     valid = true
                 end
             end
